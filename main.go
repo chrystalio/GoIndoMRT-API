825d12db6ed0ab5c1b72f5d4d786512e0123e036
Chrystalio <chrystalio.k@outlook.com>
2024-12-30 02:38:54 +0700

    Initialize main router and integrate station module with Gin framework

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"github.com/chrystalio/GoIndoMRT-API/modules/station"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	InitiateRouter()
+}
+
+func InitiateRouter() {
+	var (
+		router = gin.Default()
+		api    = router.Group("/v1/api")
+	)
+
+	station.Initiate(api)
+
+	router.Run(":3000")
 }
