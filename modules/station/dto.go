4f0bc897ccbfbb2a846c98911459dee17855750c
Chrystalio <chrystalio.k@outlook.com>
2025-01-01 01:39:43 +0700

    Add additional fields to Station and StationResponse DTOs for enhanced station data

--- modules/station/dto.go
+++ modules/station/dto.go
@@ -1,13 +1,19 @@
 package station
 
 type Station struct {
-	Id   string `json:"nid"`
-	Name string `json:"title"`
+	Id       string `json:"nid"`
+	Name     string `json:"title"`
+	Order    string `json:"urutan"`
+	LocalMap string `json:"peta_lokalitas"`
+	Banner   string `json:"banner"`
 }
 
 type StationResponse struct {
-	Id   string `json:"nid"`
-	Name string `json:"title"`
+	Id       string `json:"nid"`
+	Name     string `json:"title"`
+	Order    string `json:"urutan"`
+	LocalMap string `json:"local_map"`
+	Banner   string `json:"banner"`
 }
 
 type Schedule struct {
