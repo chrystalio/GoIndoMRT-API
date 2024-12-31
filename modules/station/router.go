fbf6fc24a461c57b9c516ce6638f851b231cac48
Chrystalio <chrystalio.k@outlook.com>
2024-12-31 15:28:33 +0700

    Add endpoint to retrieve schedules by station ID

--- modules/station/router.go
+++ modules/station/router.go
@@ -15,6 +15,10 @@ func Initiate(router *gin.RouterGroup) {
 	station.GET("", func(c *gin.Context) {
 		GetAllStations(c, stationService)
 	})
+
+	station.GET("/:id", func(c *gin.Context) {
+		CheckSchedulesByStation(c, stationService)
+	})
 }
 
 func GetAllStations(c *gin.Context, service Service) {
@@ -35,4 +39,25 @@ func GetAllStations(c *gin.Context, service Service) {
 		Data:    datas,
 	})
 
+}
+
+func CheckSchedulesByStation(c *gin.Context, service Service) {
+	id := c.Param("id")
+
+	datas, err := service.CheckSchedulesByStation(id)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, response.APIResponse{
+			Success: false,
+			Message: err.Error(),
+			Data:    nil,
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, response.APIResponse{
+		Success: true,
+		Message: "Successfully get schedules",
+		Data:    datas,
+	})
 }
